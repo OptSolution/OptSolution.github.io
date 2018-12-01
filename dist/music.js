007385fcbfbe3038e0d32d3ca74a7fe1592b5098
OptSolution <mr_cwang@foxmail.com>
2018-12-01 23:59:54 +0800

    Site updated: 2018-12-01 23:59:54

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
       {
         name: '归来',
         artist: '纵贯线',
-        url: 'https://m8.music.126.net/20181201182211/7654a21484627b6a9069520f67f71824/ymusic/4f80/ebdc/2a5f/1f1168ebd6efcaa7b72145204b8c2903.mp3',
+        url: 'http://link.hhtjim.com/163/394658.mp3',
         cover: 'http://p1.music.126.net/CZQlP4Qgy2jbk22BKyDkUA==/120946279068589.jpg',
       }
     ]
