fe46cb631dc01e3c84d15dd9a72b73f6128e6356
OptSolution <mr_cwang@foxmail.com>
2018-12-20 19:03:32 +0800

    Site updated: 2018-12-20 19:03:32

--- dist/music.js
+++ dist/music.js
@@ -8,7 +8,7 @@ const ap = new APlayer({
         name: '归来',
         artist: '纵贯线',
         url: 'http://link.hhtjim.com/163/394658.mp3',
-        cover: 'http://p1.music.126.net/CZQlP4Qgy2jbk22BKyDkUA==/120946279068589.jpg',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001dFvZR0ydHNT.jpg?max_age=2592000',
       }
     ]
 });
